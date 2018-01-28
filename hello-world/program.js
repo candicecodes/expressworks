e6d34f0fca810fccca7ebf78fe4ae30e83b190c9
Candice Mayes <ca.mayes239@gmail.com>
2018-01-28 10:35:36 -0500

    expressworks nodeschool module

---
+++ hello-world/program.js
@@ -0,0 +1,10 @@
+/* HELLO WORLD EXPRESS ================== */
+
+const express = require('express');
+const port = process.argv[2];
+
+const app = express();
+app.get('/home', (req, res) => {
+    res.end('Hello World!');
+}); 
+app.listen(port);
